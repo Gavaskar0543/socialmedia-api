99275507cbcb745bc34bad978179b84f5944801a
Gavaskar <103166821+Gavaskar0543@users.noreply.github.com>
2023-07-18 08:49:41 +0000

    routeing started

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const port = 8000;
 const app = express();
 const db = require('./config/mongoose');
+app.use('/',require('./router'));
 app.listen(port,function(err){
     if(err){
         console.log('error in running server',error.message);
