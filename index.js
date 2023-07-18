7a44612ba8d7b3f7cbc94f4755d6ca6331f87a77
Gavaskar <103166821+Gavaskar0543@users.noreply.github.com>
2023-07-18 08:40:51 +0000

    server up on port 8000

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const port = 8000;
+const app = express();
+const db = require('./config/mongoose');
+app.listen(port,function(err){
+    if(err){
+        console.log('error in running server',error.message);
+        return;
+    }
+    console.log(`server up on port ${port}`);
+})
