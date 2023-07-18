483e312507859142ed618d573d74ace24080301a
Gavaskar <103166821+Gavaskar0543@users.noreply.github.com>
2023-07-18 08:43:19 +0000

    db connected

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
-mongoose.connect();
+mongoose.connect('mongodb+srv://gavaskar:V.gu2PSzC7cLWLa@cluster0.9lt2ej0.mongodb.net/?retryWrites=true&w=majority')
 
 const db = mongoose.connection;
 
 db.on('error',console.error.bind(console,"error in connecting database"));
 
-db.once(open,function(){
+db.once('open',function(){
     console.log('MongoDb connected Succesfully!');
 });
 module.exports = db;
